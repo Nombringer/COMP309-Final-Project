ebeadc5436fec11e7dc451920cf3f8ffbb629335
Nombringer <FREDtheday@gmail.com>
2024-10-28 23:21:37 +1300

    training updated

--- cnnproject/modeling/train.py
+++ cnnproject/modeling/train.py
@@ -17,6 +17,9 @@ from cnnproject.config import MODELS_DIR, PROCESSED_DATA_DIR
 from cnnproject import dataset
 from cnnproject.dataset import ImageDataset
 
+from torchvision.models.convnext import LayerNorm2d
+from functools import partial
+
 
 app = typer.Typer()
 
@@ -38,15 +41,67 @@ def main(
         )
     logger.info(f"Using {device} device")
 
-    model= basicNN(num_classes=3).to(device)
-    print(model)
+   
+    
+    trainCNN(device)
 
-    #Load downscaled dataset
-    with open(PROCESSED_DATA_DIR / 'train_dataset_downscaled.pkl', 'rb') as f:
-        train_dataset = pickle.load(f)
-    train_basic_NN(device)
+    #train_basic_NN(device)
+
+def set_pretrained_CNN():
+    logger.info("Downloading pretrained CNN model")
+    weights = models.ConvNeXt_Large_Weights
+    model = models.convnext_large(weights)
+    torch.save(model, MODELS_DIR / "convnext_large.pth")
+    torch.save(weights, MODELS_DIR / "ConvNeXt_Large_Weights.pth")
+    logger.success(f"Model saved to {MODELS_DIR / 'ConvNeXt_Large_Weights.pth'}")
+
+def trainCNN(device):
+    logger.info("Training CNN model")
+    #setup model
+    logger.info("Loading pretrained model")
+    model = models.convnext_large(weights = models.ConvNeXt_Large_Weights)
+    #save the model
+    torch.save(model, MODELS_DIR / "convnext_large_pretrained.pth")
+
+    for param in model.parameters():
+        param.requires_grad = False
 
     
+    
+    #Adjusting the final classifier layer requires a finnickity workaround by importing the LayerNorm2d class from the convnext module directly
+    model.classifier = nn.Sequential(
+        LayerNorm2d(1536, eps=1e-06, elementwise_affine=True),
+        nn.Flatten(start_dim=1, end_dim=-1),
+        nn.Linear(1536, 3)
+    )
+    print(model)
+
+    model = model.to(device)
+
+    logger.success("Model ready for training")
+
+    with open(PROCESSED_DATA_DIR / 'train_dataset_downscaled.pkl', 'rb') as f:
+        train_dataset = pickle.load(f)
+    train_dataloader = DataLoader(train_dataset, batch_size=64)
+
+    with open(PROCESSED_DATA_DIR / 'val_dataset_downscaled.pkl', 'rb') as f:
+        test_dataset = pickle.load(f)
+    test_dataloader = DataLoader(test_dataset, batch_size=64)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    batch_size = 64
+    epochs = 10
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, batch_size)
+        validate(test_dataloader, model, loss_fn)
+    logger.success("Done!")
+    #save model
+    torch.save(model, MODELS_DIR / "CNN_10_epochs.pth")
+
+
 def train_basic_NN(device):
     model= basicNN(num_classes=3).to(device)
     print(model)
@@ -82,7 +137,8 @@ def train(dataloader, model, loss_fn, optimizer, batch_size):
     # Set the model to training mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
     model.train()
-    for batch, (X, y) in enumerate(dataloader):
+    stream = tqdm(dataloader)
+    for batch, (X, y) in enumerate(stream):
         # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -97,17 +153,15 @@ def train(dataloader, model, loss_fn, optimizer, batch_size):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 def validate(dataloader, model, loss_fn):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
-
+    stream = tqdm(dataloader)
     # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
     # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
-        for X, y in dataloader:
+        for X, y in stream:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
